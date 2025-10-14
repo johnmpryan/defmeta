2715c781bfb86e551b4efb45b0eda7cd0b2e2daa
John Ryan <johnmpryan@gmail.com>
2025-10-14 12:55:58 -0400

    Add Django admin interface for models

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Subreddit, SubredditDailyStats
 
-# Register your models here.
+@admin.register(Subreddit)
+class SubredditAdmin(admin.ModelAdmin):
+    list_display = ['name', 'population', 'subscribers_count_display', 'date_created']
+    search_fields = ['name']
+    list_editable = ['population']
+    
+    def subscribers_count_display(self, obj):
+        latest = obj.subredditdailystats_set.order_by('-date_created').first()
+        return latest.subscribers_count if latest else 'N/A'
+    subscribers_count_display.short_description = 'Latest Subscribers'
+
+@admin.register(SubredditDailyStats)
+class SubredditDailyStatsAdmin(admin.ModelAdmin):
+    list_display = ['subreddit', 'subscribers_count', 'posts_count', 'avg_post_score', 'date_created']
+    list_filter = ['subreddit', 'date_created']
+    readonly_fields = ['date_created', 'date_updated']
