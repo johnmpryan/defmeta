3fac0850627a2b1beef5e0abfbe3c7181aec5544
John Ryan <johnmpryan@gmail.com>
2025-10-20 10:38:51 -0400

    adding Post to Django adim

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Subreddit, SubredditDailyStats
+from .models import Subreddit, SubredditDailyStats, Post
 
 @admin.register(Subreddit)
 class SubredditAdmin(admin.ModelAdmin):
@@ -17,3 +17,31 @@ class SubredditDailyStatsAdmin(admin.ModelAdmin):
     list_display = ['subreddit', 'subscribers_count', 'posts_count', 'avg_post_score', 'date_created']
     list_filter = ['subreddit', 'date_created']
     readonly_fields = ['date_created', 'date_updated']
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'subreddit',
+        'title_preview',
+        'author',
+        'created_local',
+        'score',
+        'upvote_ratio',
+        'num_comments',
+        'engagement_collected',
+    ]
+    list_filter = [
+        'subreddit',
+        'engagement_collected',
+        'is_removed',
+        'created_local',
+    ]
+    search_fields = ['title', 'author', 'reddit_id']
+    readonly_fields = ['reddit_id', 'created_utc', 'created_local', 'date_created', 'date_updated']
+    date_hierarchy = 'created_local'
+    list_per_page = 50  # More posts per page since you'll have many
+    
+    def title_preview(self, obj):
+        """Show first 60 chars of title"""
+        return obj.title[:60] + '...' if len(obj.title) > 60 else obj.title
+    title_preview.short_description = 'Title'
