18c38db2d8ed58254f8140edddae1469724b7829
John Ryan <johnmpryan@gmail.com>
2025-10-20 09:22:00 -0400

    django admin add timezone and region to list view

--- tracker/admin.py
+++ tracker/admin.py
@@ -3,9 +3,9 @@ from .models import Subreddit, SubredditDailyStats
 
 @admin.register(Subreddit)
 class SubredditAdmin(admin.ModelAdmin):
-    list_display = ['name', 'population', 'subscribers_count_display', 'date_created']
+    list_display = ['name', 'population','timezone','region', 'subscribers_count_display','residents_over_18']
     search_fields = ['name']
-    list_editable = ['population']
+    list_editable = ['population','residents_over_18','timezone','region']
     
     def subscribers_count_display(self, obj):
         latest = obj.subredditdailystats_set.order_by('-date_created').first()
