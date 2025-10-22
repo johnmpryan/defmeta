1c7a92db356974ea29bdd297c5bc09b3ebfc83e9
John Ryan <johnmpryan@gmail.com>
2025-10-21 21:00:20 -0400

    adding additional reddit fields to post

--- tracker/models.py
+++ tracker/models.py
@@ -29,11 +29,11 @@ class SubredditDailyStats(models.Model):
     avg_upvote_ratio = models.FloatField(null=True, blank=True)
     total_comments = models.IntegerField(null=True, blank=True)
     engagement_collected = models.BooleanField(default=False)  # Track if we've collected this yet
-    
+
     class Meta:
         db_table = 'subreddits_daily_stats'
         ordering = ['-date_created']
-    
+
     def __str__(self):
         return f"{self.subreddit.name} - {str(self.date_created)}"
 
@@ -42,7 +42,20 @@ class Post(models.Model):
     reddit_id = models.CharField(max_length=20, unique=True)
     title = models.TextField()
     author = models.CharField(max_length=100)
-    body = models.TextField(null=True, blank=True)  # or 'selftext' to match Reddit's naming
+    body = models.TextField(null=True, blank=True)
+    
+    # Content type fields
+    url = models.URLField(max_length=500, null=True, blank=True)
+    is_self = models.BooleanField(default=False)
+    permalink = models.CharField(max_length=300, null=True, blank=True)
+    
+    # Metadata fields
+    over_18 = models.BooleanField(default=False)
+    spoiler = models.BooleanField(default=False)
+    stickied = models.BooleanField(default=False)
+    locked = models.BooleanField(default=False)
+    link_flair_text = models.CharField(max_length=100, null=True, blank=True)
+    distinguished = models.CharField(max_length=20, null=True, blank=True)
     
     # Timestamp fields
     created_utc = models.DateTimeField()  # UTC timestamp from Reddit
@@ -54,7 +67,6 @@ class Post(models.Model):
     num_comments = models.IntegerField(null=True, blank=True)
     is_removed = models.BooleanField(default=False)
     engagement_collected = models.BooleanField(default=False)
-    
     
     # Django timestamps
     date_created = models.DateTimeField(auto_now_add=True)
